23227cd90e3ff0e11d672ed7384cb5131658994a
Alan Cristian <lanzn16@outlook.com>
2022-03-11 00:52:11 -0300

    add cli with cobra

--- main.go
+++ main.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"database/sql"
-
-	db2 "github.com/alancrist/nft-marktplace-service/adapters/db"
-	"github.com/alancrist/nft-marktplace-service/application"
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/alancrist/nft-marktplace-service/cmd"
 
 func main() {
-	db, _ := sql.Open("sqlite3", "sqlite.db")
-	productDbAdapter := db2.NewProductDb(db)
-	productService := application.NewProductService(productDbAdapter)
-	product, _ := productService.Create("Product", 30)
-
-	productService.Enable(product)
+	cmd.Execute()
 }
